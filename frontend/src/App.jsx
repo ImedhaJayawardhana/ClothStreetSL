5c4be22e3cfff07508243d99a5a51e29e9d0b2f5
Imedha Jayawardhana <imedaindeewari@gmail.com>
2026-03-08 16:20:19 +0530

    Implementing Nav bar and Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useAuth } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 
 function PrivateRoute({ children }) {
   const { user } = useAuth();
@@ -11,10 +13,16 @@ function PrivateRoute({ children }) {
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
