2828d04581cac62084491d04e2ea6e4461037bed
Edwardfrcullen <kavindaroids123@gmail.com>
2026-03-21 21:09:32 +0530

    CI cd issue

--- backend/firebase/admin.py
+++ backend/firebase/admin.py
@@ -2,7 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {"storageBucket": "clothstreet-3a86c.firebasestorage.app"})
+firebase_admin.initialize_app(
+    cred, {"storageBucket": "clothstreet-3a86c.firebasestorage.app"}
+)
 
 db = firestore.client()
 bucket = storage.bucket()
