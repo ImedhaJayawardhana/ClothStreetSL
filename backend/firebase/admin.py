509d0badf411c8ced1e7877e9197c802509e7b82
MarioOckersz <marioockersz16@gmail.com>
2026-03-22 20:05:39 +0530

    system changend to cloude deployment

--- backend/firebase/admin.py
+++ backend/firebase/admin.py
@@ -1,7 +1,17 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
-cred = credentials.Certificate("serviceAccountKey.json")
+if os.path.exists("serviceAccountKey.json"):
+    cred = credentials.Certificate("serviceAccountKey.json")
+elif os.environ.get("FIREBASE_CREDENTIALS"):
+    cert_dict = json.loads(os.environ.get("FIREBASE_CREDENTIALS", "{}"))
+    cred = credentials.Certificate(cert_dict)
+else:
+    # Fallback to default application credentials if none exist
+    cred = credentials.ApplicationDefault()
+
 firebase_admin.initialize_app(
     cred, {"storageBucket": "clothstreet-3a86c.firebasestorage.app"}
 )
